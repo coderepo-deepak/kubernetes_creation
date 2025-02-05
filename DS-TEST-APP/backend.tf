0a77f935b689a7015d8a08434a5ca0f216242cad
coderepo-deepak <sdpk.ks2@gmail.com>
2025-02-05 18:51:32 +0530

    Updating Secreats

--- DS-TEST-APP/backend.tf
+++ DS-TEST-APP/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "eksterrabucket"
+    bucket = "ds-test-app-tf-bucket"
     key    = "backend/ToDo-App.tfstate"
     region = "us-east-1"
     dynamodb_table = "dynamoDB-terra"
