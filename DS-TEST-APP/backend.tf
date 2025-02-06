00d46504347abd7c2254f9472c4f861cffd28785
coderepo-deepak <sdpk.ks2@gmail.com>
2025-02-06 15:29:16 +0530

    EKS Creation

--- DS-TEST-APP/backend.tf
+++ DS-TEST-APP/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "deepaks-test"
+    bucket = "my-ds-test"
     key    = "backend/ToDo-App.tfstate"
     region = "us-west-2"
     #dynamodb_table = "dynamoDB-terra"
